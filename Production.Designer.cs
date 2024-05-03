a20aea6f6b30b011073fd17a05c179568278d4dc
Jude Hashane <jude@leadlanka.lk>
2024-05-03 18:10:12 +0530

    Add project files.

---
+++ Production.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace QuietAttic
+{
+    partial class Production
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(542, 117);
+            panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ActiveCaption;
+            textBox1.Location = new Point(203, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            textBox1.Text = " Add Production";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Location = new Point(39, 173);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(463, 349);
+            panel2.TabIndex = 0;
+            // 
+            // Production
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(542, 565);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "Production";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ProductionAdd";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox textBox1;
+        private Panel panel2;
+    }
+}
